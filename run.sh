7d11f870337e9e0b902e908d7c71cbe82b2b76c2
mjonyh <mjonyh@gmail.com>
2020-08-11 16:52:02 +0600

    updated

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+rm ~/Downloads/Global_Mobility_Report*
 open https://www.google.com/covid19/mobility/
 echo "download the csv file and place it in the root directory"
 read ok
 
+cp ~/Downloads/Global_Mobility_Report.csv data/mobility.csv
+cat data/mobility.csv | grep country > data/test.csv && cat data/mobility.csv | grep Bangladesh >> data/test.csv
+
 python3 compute.py 30
 python3 compute.py 90
+
 python3 rt.py 1
 python3 rt.py 2
+
 python3 final_plot.py
+python3 owid_data.py

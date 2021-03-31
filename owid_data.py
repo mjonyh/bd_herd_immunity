a84c5346188320118f4d9e2ca384f70af9612d12
mjonyh <mjonyh@gmail.com>
2021-03-31 07:22:40 +0600

    dirty merged cleaned up

--- owid_data.py
+++ owid_data.py
@@ -2,20 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns;sns.set()
 
-# df = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
-<<<<<<< HEAD
-df = pd.read_csv('owid-covid-data.csv')
+df = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
+# df = pd.read_csv('owid-covid-data.csv')
 df = df[df['location']=='Bangladesh']
 df['date'] = pd.to_datetime(df['date'])
 
 df.to_csv('data/tpr.csv', index=False)
 
-=======
-df = pd.read_csv('data/owid-covid-data.csv')
-df = df[df['location']=='Bangladesh']
-df['date'] = pd.to_datetime(df['date'])
-
->>>>>>> b524199def02b9575a7003c77986bd21ed31654b
 fig, ax1 = plt.subplots(1)
 
 color = 'tab:blue'
@@ -37,19 +30,23 @@ ax2.set_ylim(0, 27)
 ax2.set_ylabel("Positive Test Rate (in %)",color=color)
 ax2.tick_params(axis='y',labelcolor=color)
 
-<<<<<<< HEAD
 ax2.tick_params(axis='x', rotation=45)
 
 ax2.legend(handles=[l1, l2, l3], loc=1, framealpha=0.5)
-=======
-
-ax2.legend(handles=[l1, l2, l3], loc=2)
->>>>>>> b524199def02b9575a7003c77986bd21ed31654b
 fig.tight_layout()
+
+
+# df_rt_1 = pd.read_csv('data/rt_1.csv')
+# df_rt_1['Date'] = pd.to_datetime(df_rt_1['Date'])
+# df_rt_1['smooth_ml'] = df_rt_1['ML'].rolling(7).mean()
+
+# fig2, axs = plt.subplots(1)
+# df.plot(x='date', y='reproduction_rate', ax=axs)
+# df_rt_1.plot(x='Date', y='ML', ax=axs)
+# df_rt_1.plot(x='Date', y='smooth_ml', ax=axs)
 plt.show()
 
 
-<<<<<<< HEAD
 # import plotly.graph_objects as go
 
 # fig = go.Figure()
@@ -138,95 +135,3 @@ plt.show()
 
 # chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
 # py.plot(fig, filename='Positive Test Rate')
-=======
-
-
-import plotly.graph_objects as go
-
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=df['date'],
-    y=df['positive_rate']*100,
-    name="Positive Test Rate (in %)"
-))
-
-
-fig.add_trace(go.Scatter(
-    x=df['date'],
-    y=df['new_cases'],
-    name="Daily Confirmed Cases",
-    yaxis="y2"
-))
-
-fig.add_trace(go.Scatter(
-    x=df['date'],
-    y=df['new_tests'],
-    name="Daily Tests",
-    yaxis="y3"
-))
-
-
-# Create axis objects
-fig.update_layout(
-    xaxis=dict(
-        domain=[0, 0.75]
-    ),
-    yaxis=dict(
-        title="Positive Test Rate (in %)",
-        titlefont=dict(
-            color="#1f77b4"
-        ),
-        tickfont=dict(
-            color="#1f77b4"
-        ),
-        range=[0,27],
-
-    ),
-    yaxis2=dict(
-        title="Daily Confirmed Cases",
-        titlefont=dict(
-            color="#d62728"
-        ),
-        tickfont=dict(
-            color="#d62728"
-        ),
-        anchor="free",
-        overlaying="y",
-        side="right",
-        position=0.85,
-        range=[0,5400],
-    ),
-    yaxis3=dict(
-        title="Daily Tests",
-        titlefont=dict(
-            color="green"
-        ),
-        tickfont=dict(
-            color="green"
-        ),
-        anchor="x",
-        overlaying="y",
-        side="right",
-        range=[0,27000],
-    )
-)
-
-# Update layout properties
-fig.update_layout(
-    title_text="Positive Test Rate",
-    # width=800,
-)
-
-
-fig.show()
-
-import chart_studio
-import chart_studio.plotly as py
-
-username='mjonyh'
-api_key='VVmftIeOEKQFkLHcldpQ'
-
-chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
-py.plot(fig, filename='Positive Test Rate')
->>>>>>> b524199def02b9575a7003c77986bd21ed31654b
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 # download the mobility data from google and process for Bangladesh
 wget https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip
 unzip Region_Mobility_Report_CSVs.zip
@@ -20,21 +19,5 @@ python3 rt.py 1
 python3 rt.py 2
 
 ### Plot the data
-=======
-rm ~/Downloads/Global_Mobility_Report*
-open https://www.google.com/covid19/mobility/
-echo "download the csv file and place it in the root directory"
-read ok
-
-cp ~/Downloads/Global_Mobility_Report.csv data/mobility.csv
-cat data/mobility.csv | grep country > data/test.csv && cat data/mobility.csv | grep Bangladesh >> data/test.csv
-
-python3 compute.py 30
-python3 compute.py 90
-
-python3 rt.py 1
-python3 rt.py 2
-
->>>>>>> b524199def02b9575a7003c77986bd21ed31654b
 python3 final_plot.py
 python3 owid_data.py
